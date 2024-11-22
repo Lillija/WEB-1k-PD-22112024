33e43ac9d0796ee9a076bdad1d7e21b58e3926d1
Lillija <asikeligalillija@gmail.com>
2024-11-22 10:29:27 +0200

    Add files via upload

---
+++ WEB-1k-PD-22112024/WEB-1k-PD-22112024/basics.js
@@ -0,0 +1,15 @@
+let personAge = 17;
+function testNum(personAge) {
+    let result;
+    if (17 >=18) {
+      result = 'Pilngadīgs';
+    } else {
+      result = 'Nepilngadīgs';
+    }
+    return result;
+  }
+  
+  console.log(testNum(personAge));
+  const namesArray = ['Jānis', 'Anna', 'Zaiga'];
+  console.log(namesArray);
+  console.log("Labdien, Elza!");
---
+++ WEB-1k-PD-22112024/WEB-1k-PD-22112024/calculator.js
@@ -0,0 +1,4 @@
+let firstnumberInput = document.querySelector("1st-number-input");
+let plusorminusInput = document.querySelector("plusorminus");
+let secondtnumberInput = document.querySelector("2nd-number-input");
+let calculateButton = document.querySelector("equals")
